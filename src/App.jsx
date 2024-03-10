672e7993b950e7ec5cc128a2b7242aa1e9976677
Sho <shohihi@hotmail.com>
2024-03-10 23:54:15 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Card from "./components/Card"
+import Navbar from "./components/Navbar"
+import data from "./data"
+
+export default function App() {
+  const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
+  return (
+    <>
+      <Navbar />
+      {cards}
+    </>
+  )
+}
+

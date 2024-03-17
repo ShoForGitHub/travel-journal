b1e247ec3f6d9b43c978fbfb1e452d733311e880
Sho <shohihi@hotmail.com>
2024-03-17 18:02:06 +0900

    Fix photos not showing in github pages

--- src/data.jsx
+++ src/data.jsx
@@ -7,7 +7,7 @@ export default [
         startDate: "11 Feb, 2024",
         endDate: "11 Feb, 2024",
         introduction: "川面に映る影が双円を描き「眼鏡」に見えます。東京の「日本橋」、山口の「錦帯橋」と並び日本三名橋に数えられます。長崎のランタンフェスティバルの時には、眼鏡橋周辺も提灯で華やかに飾られ普段以上に多くの人で賑わいます。",
-        imageUrl: "./src/assets/megane_bridge.jpg",
+        imageUrl: "./public/megane_bridge.jpg",
         alt: "長崎ランタン・フェスティバル時の眼鏡橋",
     },
 
@@ -19,7 +19,7 @@ export default [
         startDate: "6 Oct, 2023",
         endDate: "7 Oct, 2023",
         introduction: "雲仙温泉を代表する観光名所・地獄温泉。 硫黄の香りが立ち込め、地の底から吹き出す蒸気と熱気が辺り一面を覆い尽くす光景は、まさに地獄そのもの。ここはキリシタン殉教の舞台になったところでも知られています。",
-        imageUrl: "./src/assets/unsen_hell.jpg",
+        imageUrl: "./public/unsen_hell.jpg",
         alt: "「立入禁止、地獄内は危険です。－環境省」の看板",
     },
 
@@ -31,7 +31,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         introduction: "岡山藩2代目藩主・池田綱政(つなまさ)が自ら憩いの場として築いた大庭園。季節の花が彩る広々とした園内は江戸時代さながらの別世界。金沢の兼六園、水戸の偕楽園とあわせて「日本三名園」と称されています。",
-        imageUrl: "./src/assets/okayama_gorakuen.jpg",
+        imageUrl: "./public/okayama_gorakuen.jpg",
         alt: "後楽園の風景",
     },
 ]
